8efa24990bbc45c4388065a1eed8afcdbfa879b0
Stefano Miceli <Rende>
2023-02-03 17:25:38 +0100

    new project Promemoria

--- src/main/java/com/example/promemoria/Service/UtenteService.java
+++ src/main/java/com/example/promemoria/Service/UtenteService.java
@@ -3,9 +3,10 @@ package com.example.promemoria.Service;
 import com.example.promemoria.Entity.Utente;
 import com.example.promemoria.Repository.UntenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UtenteService {
     @Autowired
     private UntenteRepository untenteRepository;
